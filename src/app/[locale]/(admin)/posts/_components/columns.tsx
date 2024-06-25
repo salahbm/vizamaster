c54fe10df299199d66df0975e161a1211eded16b
salahbm <salahbm.001@gmail.com>
2024-06-26 01:01:40 +0900

    [add]: column, data-table

---
+++ src/app/[locale]/(admin)/posts/_components/columns.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown, MoreHorizontal, Pencil } from 'lucide-react';
+
+import Link from 'next/link';
+import { Badge } from '@/components/ui/badge';
+import { cn } from '@/lib/utils';
+import { Country } from '@prisma/client';
+
+export const columns: ColumnDef<Country>[] = [
+  {
+    accessorKey: 'emoji',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      //   const price = parseFloat(row.getValue('price') || '0');
+
+      return <span>price</span>;
+    },
+  },
+  {
+    accessorKey: 'isNew',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const isPublished = row.getValue('isNew') || false;
+
+      return (
+        <Badge className={cn('bg-slate-500', isPublished && 'bg-sky-700')}>
+          {isPublished ? 'Published' : 'Draft'}
+        </Badge>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    header: () => {
+      return <p>Action</p>;
+    },
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <Button variant={'ghost'} className="h-4 w-8 p-0">
+          <Link href={`/teacher/courses/${id}`}>
+            <Pencil className="h-4 w-4 mr-2 hover:text-sky-700" />
+          </Link>
+        </Button>
+        //     <DropdownMenu>
+        //     <DropdownMenuTrigger asChild>
+        //         <Button variant={'ghost'} className="h-4 w-8 p-0">
+        //             <span className="sr-only">Open menu</span>
+
+        //             <MoreHorizontal className="h-4 w-4" />
+        //         </Button>
+        //     </DropdownMenuTrigger>
+
+        //     <DropdownMenuContent>
+        //         <Link href={`/teacher/courses/${id}`}>
+        //             <DropdownMenuItem>
+        //                 <Pencil className="h-4 w-4 mr-2" />
+        //                 Edit
+        //             </DropdownMenuItem>
+        //         </Link>
+        //     </DropdownMenuContent>
+        // </DropdownMenu>
+      );
+    },
+  },
+];
