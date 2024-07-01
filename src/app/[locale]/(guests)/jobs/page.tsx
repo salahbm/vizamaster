a8617148c5cdf80e79e038493cffa5b56cb61201
salahbm <salahbm.001@gmail.com>
2024-07-01 21:05:26 +0900

    [fix]: nav in blog

--- src/app/[locale]/(guests)/jobs/page.tsx
+++ src/app/[locale]/(guests)/jobs/page.tsx
@@ -1,6 +1,7 @@
 import CountryCard from '@/components/_jobs/country-card';
 import MyGlobe from '@/components/shared/globe';
 import EmptyState from '@/components/shared/no-data';
+import SectionHeader from '@/components/shared/SectionHeader';
 import { fetchAllCountries } from '@/hooks/admin/fetch-country';
 import { Country } from '@prisma/client';
 
@@ -13,6 +14,13 @@ const Jobs = async () => {
     <div>
       {countries ? (
         <div className="my-35 px-4 flex flex-wrap items-center justify-center gap-4">
+          <SectionHeader
+            headerInfo={{
+              title: `NEWS & Vacancies`,
+              subtitle: `Latest News & Vacancies`,
+              description: `Search for internships, jobs, and more. Select the country you are interested in.`,
+            }}
+          />
           {countries.map((item) => (
             <CountryCard key={item.id} country={item as Country} />
           ))}
--- src/components/Blog/BlogItem.tsx
+++ src/components/Blog/BlogItem.tsx
@@ -27,13 +27,13 @@ const BlogItem = ({ blog }: { blog: Job }) => {
         viewport={{ once: true }}
         className="animate_top rounded-lg bg-white p-4 pb-9 shadow-solid-8 dark:bg-blacksection shadow-xl "
       >
-        <Link href={`/blog/`} className="relative block aspect-[368/239] ">
+        <Link href={`/jobs`} className="relative block aspect-[368/239] ">
           <Image src={imgUrl} alt={title!!} fill className="rounded-lg" />
         </Link>
 
         <div className="px-4">
           <h3 className="mb-3.5 mt-7.5 line-clamp-2 inline-block text-lg font-medium text-black duration-300 hover:text-primary dark:text-white dark:hover:text-primary xl:text-itemtitle2">
-            <Link href={`/blog/blog-details`}>{`${name.slice(0, 40)}...`}</Link>
+            <Link href={`/jobs`}>{`${name.slice(0, 40)}...`}</Link>
           </h3>
           <p className="line-clamp-3">{title}</p>
         </div>
