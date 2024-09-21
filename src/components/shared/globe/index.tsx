ba7a81dc6c089c2b0c83440442130ee0607733f5
salahbm <salahbm.001@gmail.com>
2024-09-21 12:01:30 +0500

    [upd]: glb

--- src/components/shared/globe/index.tsx
+++ src/components/shared/globe/index.tsx
@@ -40,7 +40,7 @@ const MyGlobe = () => {
 
   return (
     <>
-      {!webGLSupported ? (
+      {webGLSupported ? (
         <div className="relative w-full pb-12">
           <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
 
